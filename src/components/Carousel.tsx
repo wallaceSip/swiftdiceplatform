897fc5d4b47bab71774f2efcf5f24c10f1527d4f
Wallace <wallacesip@gmail.com>
2024-02-17 18:03:31 -0300

    Carousel update

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,8 +7,7 @@ import { TokenListingBanner } from '../sections/Dashboard/NewBanner';
 
 export function NewsCarousel() {
   const banners = [
-    <WelcomeBanner key={1} />,
-    <TokenListingBanner key={2} />,
+    <WelcomeBanner key={1} />
   ];
 
   const settings = {
