25077d010b16157e891d3430484fe506c54d42f9
Wallace <wallacesip@gmail.com>
2024-02-21 02:18:02 -0300

    mainPage update --hotfix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import mascotImage from '../../../public/mascot.png';
+import mascotImage from '../../public/mascot.png';
 
 const fadeIn = keyframes`
   from {
